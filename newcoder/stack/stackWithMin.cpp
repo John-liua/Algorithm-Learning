d97c50c1c71e8e9adb65590746493ed83a5fae9f
John Liu <89292585+John-liua@users.noreply.github.com>
2022-07-28 02:52:11 +0800

    Add files via upload

---
+++ newcoder/stack/stackWithMin.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    stack<int> s1;
+    stack<int> s2;
+    void push(int value) {
+        s1.push(value);
+        if(s2.empty()||value<s2.top())
+            s2.push(value);
+        else
+            s2.push(s2.top());
+    }
+    void pop() {
+        s1.pop();
+        s2.pop();
+    }
+    int top() {
+        return s1.top();
+    }
+    int min() {
+        return s2.top();
+    }
+};
