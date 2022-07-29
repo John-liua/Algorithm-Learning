2568f7726fe8680642552c53f00804eb828da100
John Liu <89292585+John-liua@users.noreply.github.com>
2022-07-30 02:15:07 +0800

    Update recursion.cpp

--- newcoder/recursion/recursion.cpp
+++ newcoder/recursion/recursion.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    void recursion(vector<vector<int>> &res,vector<int> &num,int index)
+    {
+        if(index==num.size())
+            res.push_back(num);
+        else
+        {
+            for(int i=index;i<num.size();i++)
+            {
+                swap(num[i],num[index]);
+                recursion(res, num, index+1);
+                swap(num[i],num[index]);
+            }
+        }
+    }
     vector<vector<int> > permute(vector<int> &num) {
-}
+        vector<vector<int>> res;
+        sort(num.begin(), num.end());
+        recursion(res, num, 0);
+        return res;
+    }
 };
