4465700df9048a4fe164496af6d9d3c902e115b8
John Liu <89292585+John-liua@users.noreply.github.com>
2022-08-25 09:56:47 +0800

    Add files via upload

---
+++ newcoder/double pointer/judgeReverse.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     * 
+     * @param str string字符串 待判断的字符串
+     * @return bool布尔型
+     */
+    bool judge(string str) {
+        int left=0;
+        int right=str.length()-1;
+        while(left<right){
+            if(str[left]!=str[right])
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
